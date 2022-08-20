01cc3f569e2f4386c7efe8e89e7a1aeb36eb6e52
sol <osunpine@gmail.com>
2022-08-20 17:03:25 +0900

    Devtools

--- base-blog-em/src/App.jsx
+++ base-blog-em/src/App.jsx
@@ -2,6 +2,7 @@ import { Posts } from "./Posts";
 import "./App.css";
 
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 // Clent 추가 후, Provider 추가
 // Client가 가지고 있는 캐시와 기본 옵션(React Query Hooks)을 자식 컴포넌트가 사용할 수 있도록 전달
@@ -15,6 +16,8 @@ function App() {
         <h1>Blog Posts</h1>
         <Posts />
       </div>
+      {/* Devtools : 개발 중인 모든 쿼리 상태 표시 */}
+      <ReactQueryDevtools />
     </QueryClientProvider>
   );
 }
