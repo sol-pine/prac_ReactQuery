c2df9ca39048a5b3429a527e03f0444dab88cbf0
sol <osunpine@gmail.com>
2022-09-02 17:42:50 +0900

    custom query hooks

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -1,15 +1,20 @@
+import { useQuery } from 'react-query';
+
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
 import { useCustomToast } from '../../app/hooks/useCustomToast';
 
 // for when we need a query function for useQuery
-// async function getTreatments(): Promise<Treatment[]> {
-//   const { data } = await axiosInstance.get('/treatments');
-//   return data;
-// }
+async function getTreatments(): Promise<Treatment[]> {
+  const { data } = await axiosInstance.get('/treatments');
+  return data;
+}
 
 export function useTreatments(): Treatment[] {
   // TODO: get data from server via useQuery
-  return [];
+  // data destructuring
+  // useQuery(쿼리키, 쿼리함수)
+  const { data } = useQuery(queryKeys.treatments, getTreatments);
+  return data;
 }
