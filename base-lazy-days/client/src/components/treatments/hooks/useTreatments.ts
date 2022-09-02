caab88aa5c07df394353ee41e5a6df5e69a99315
sol <osunpine@gmail.com>
2022-09-02 23:12:49 +0900

    onError 기본 값 설정 : 전역 에러 처리

--- base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
+++ base-lazy-days/client/src/components/treatments/hooks/useTreatments.ts
@@ -3,7 +3,6 @@ import { useQuery } from 'react-query';
 import type { Treatment } from '../../../../../shared/types';
 import { axiosInstance } from '../../../axiosInstance';
 import { queryKeys } from '../../../react-query/constants';
-import { useCustomToast } from '../../app/hooks/useCustomToast';
 
 // for when we need a query function for useQuery
 async function getTreatments(): Promise<Treatment[]> {
@@ -12,22 +11,7 @@ async function getTreatments(): Promise<Treatment[]> {
 }
 
 export function useTreatments(): Treatment[] {
-  // onError 콜백 시, toast 함수 사용
-  const toast = useCustomToast();
-
   const fallback = [];
-  // TODO: get data from server via useQuery
-  // data destructuring
-  // useQuery(쿼리키, 쿼리함수)
-  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments, {
-    onError: (error) => {
-      const title =
-        error instanceof Error
-          ? error.message
-          : 'error connecting to the server';
-      toast({ title, status: 'error' });
-    },
-  });
-
+  const { data = fallback } = useQuery(queryKeys.treatments, getTreatments);
   return data;
 }
