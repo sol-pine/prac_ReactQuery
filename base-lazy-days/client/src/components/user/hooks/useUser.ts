3f8302abbcd53f2f4cc3d447cbba7ab203797f9a
sol <osunpine@gmail.com>
2022-09-05 11:38:24 +0900

    Auth : 유저 데이터 처리

--- base-lazy-days/client/src/components/user/hooks/useUser.ts
+++ base-lazy-days/client/src/components/user/hooks/useUser.ts
@@ -1,4 +1,5 @@
 import { AxiosResponse } from 'axios';
+import { useQuery, useQueryClient } from 'react-query';
 
 import type { User } from '../../../../../shared/types';
 import { axiosInstance, getJWTHeader } from '../../../axiosInstance';
@@ -9,16 +10,16 @@ import {
   setStoredUser,
 } from '../../../user-storage';
 
-// async function getUser(user: User | null): Promise<User | null> {
-//   if (!user) return null;
-//   const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
-//     `/user/${user.id}`,
-//     {
-//       headers: getJWTHeader(user),
-//     },
-//   );
-//   return data.user;
-// }
+async function getUser(user: User | null): Promise<User | null> {
+  if (!user) return null;
+  const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
+    `/user/${user.id}`,
+    {
+      headers: getJWTHeader(user),
+    },
+  );
+  return data.user;
+}
 
 interface UseUser {
   user: User | null;
@@ -27,17 +28,22 @@ interface UseUser {
 }
 
 export function useUser(): UseUser {
-  // TODO: call useQuery to update user data from server
-  const user = null;
+  // 훅을 이용해 queryClient 가져옴
+  const queryClient = useQueryClient();
+  // 기존 유저 데이터(캐시 데이터)를 이용해 서버에서 데이터 get
+  const { data: user } = useQuery(queryKeys.user, () => getUser(user));
 
   // meant to be called from useAuth
   function updateUser(newUser: User): void {
-    // TODO: update the user in the query cache
+    // 유저가 성공적으로 로그인 했을 경우, 캐시에 유저 데이터 업데이트
+    // setQueryData(쿼리키, 업데이트데이터)
+    queryClient.setQueryData(queryKeys.user, newUser);
   }
 
   // meant to be called from useAuth
   function clearUser() {
-    // TODO: reset user to null in query cache
+    // 유저가 로그아웃하면 캐시에 있는 유저 데이터 null로 무효화
+    queryClient.setQueryData(queryKeys.user, null);
   }
 
   return { user, updateUser, clearUser };
